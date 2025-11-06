bc971f40637f3408a7077c78eb279f16be64530e
youngmin Lee <youngminlee@youngmins-MacBook-Air.local>
2025-11-06 14:53:33 +0900

    fastapi/docs로items API 테스트

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name:str
+    price:float
+    is_offer: Union[bool,None] = None
 
 @app.get("/")
 def read_root():
@@ -12,6 +19,9 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    return {"itemName":item.name , "item_id":item_id}
 
 
 # python3 -m uvicorn main:app --reload --port 5050
