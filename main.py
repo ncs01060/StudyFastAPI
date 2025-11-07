56f5bbdb8aa20a05f2cb6435916c13c6209bb649
youngmin Lee <youngminlee@youngmins-MacBook-Air.local>
2025-11-07 09:10:32 +0900

    post방식으로 회원가입 구현

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Union,Optional
+from fastapi import FastAPI,Header
+import util.type as mainType
 from util.storeAPI import userDataBase
 
 app = FastAPI()
 
-# init = userDataBase()
-# init.initDB()
+init = userDataBase()
+init.initDB()
 
-class Item(BaseModel):
-    name:str
-    price:float
-    is_offer: Union[bool,None] = None
 
 @app.get("/")
 def read_root():
@@ -23,14 +19,15 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put("/items/{item_id}")
-def update_item(item_id:int, item:Item):
+def update_item(item_id:int, item:mainType.Item):
     return {"itemName":item.name , "item_id":item_id}
 
 
-@app.get("/sigup/{name}")
-def saveUserData(name:str):
+@app.post("/sigup/")
+def saveUserData(data:mainType.User):
     db = userDataBase()
-    return db.saveData(name)
+    return db.saveData(data.name,data.password)
 
 # python3 -m uvicorn main:app --reload --port 5050
